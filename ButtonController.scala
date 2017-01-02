8943079595cc9558850e59e6b7edee09f58261d1
kmniemi <katri.niemi@aalto.fi>
2017-01-02 11:43:35 +0200

    Update ButtonController.scala

--- ButtonController.scala
+++ ButtonController.scala
@@ -1,3 +1,8 @@
+package sudoku
+
+import scala.swing._
+import scala.swing.event._
+
 class ButtonController(private var game: Game) extends ActionListener { 
   
     def actionPerformed(e: ActionEvent) {
@@ -9,7 +14,7 @@ class ButtonController(private var game: Game) extends ActionListener {
       (e.getActionCommand == "Exit") System.exit(0) 
     } else if {
       (e.getActionCommand == "Help on") game.setHelp(e.getSource.asInstanceOf[JCheckBox].isSelected)
-    } else { game.setSelectedNumber(java.lang.Integer.parseInt(e.getActionCommand))
+    } else { game.setSelectedNumber
       }
     }
   }
